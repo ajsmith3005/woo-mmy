db046e2009838aa37271457a0d306316eed8b1ec
ajsmith3005 <135562778+ajsmith3005@users.noreply.github.com>
2025-03-05 14:30:12 -0800

    adds main plugin php file with plugin header

---
+++ woo-mmy.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Plugin Name:       Woo-MMY
+ * Description:       Search products by Make, Model, and Year by using custom taxonomies to categorize them.
+ * Version:           0.1.0
+ * Author:            Andrew Smith
+ * Author URI:        https://andycodes.net/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Requires Plugins:  woocommerce
+ */
