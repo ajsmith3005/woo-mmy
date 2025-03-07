1838f8cf012d2274dcded1dcf558c0ac66636fca
ajsmith3005 <135562778+ajsmith3005@users.noreply.github.com>
2025-03-07 14:02:41 -0800

    adds action to enqueue custom scripts

--- includes/functions.php
+++ includes/functions.php
@@ -11,3 +11,23 @@ function woommy_plugin_url( $path = '' ) {
 
 	return $url;
 }
+
+// Add javascript
+function add_custom_scripts() {
+
+	// register the script
+	wp_register_script( 
+		'woommy-main-js', 
+		woommy_plugin_url('/includes/js/index.js'), 
+		array(),
+		false,
+		array(
+			'strategy' => 'defer'
+		)
+	);
+
+	//enqueue the script
+	wp_enqueue_script( 'woommy-main-js' );
+
+}
+add_action( 'wp_enqueue_scripts', 'add_custom_scripts' );
