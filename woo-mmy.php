65f8e9486b809dfbd40264be660a7d12e48753d2
ajsmith3005 <135562778+ajsmith3005@users.noreply.github.com>
2025-03-07 00:30:00 -0800

    registers the custom make model year taxonomy

--- woo-mmy.php
+++ woo-mmy.php
@@ -18,3 +18,33 @@ define( 'WOOMMY_PLUGIN' , __FILE__ );
 define( 'WOOMMY_PLUGIN_DIR', untrailingslashit( dirname( WOOMMY_PLUGIN) ) );
 
 require_once( WOOMMY_PLUGIN_DIR . '/includes/functions.php' );
+
+/**
+ *Adds custom taxonomies
+ */
+function woommy_create_taxonomies() {
+	$labels = array(
+		'name'              =>_X('Makes and Models', 'taxonomy general name', 'textdomain'),
+		'singular_name'     => _X('Make and Model', 'taxonomy singular name', 'textdomain'),
+		'search_items'      => __( 'Search Makes and Models', 'textdomain' ),
+		'all_items'         => __( 'All Makes and Models', 'textdomain' ),
+		'parent_item'       => __( 'Parent Make', 'textdomain' ),
+		'parent_item_colon' => __( 'Parent Make:', 'textdomain' ),
+		'edit_item'         => __( 'Edit Make or Model', 'textdomain' ),
+		'update_item'       => __( 'Update Make or Model', 'textdomain' ),
+		'add_new_item'      => __( 'Add New Make or Model', 'textdomain' ),
+		'new_item_name'     => __( 'New Make or Model Name', 'textdomain' ),
+		'menu_name'         => __( 'Makes and Models', 'textdomain' ),
+	);
+
+	$args = array(
+		'hierarchical' => true,
+		'labels'       => $labels,
+		'show_ui'      => true,
+		'show_in_rest' => true,
+	);
+
+	register_taxonomy( 'woommy-car-details', 'product', $args);
+}
+
+add_action( 'init', 'woommy_create_taxonomies');
