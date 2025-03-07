29a1391be90c20491e9978a51b02b0e1e5227f65
ajsmith3005 <135562778+ajsmith3005@users.noreply.github.com>
2025-03-07 12:54:14 -0800

    Adds shortcode for MMY form and populates make options

--- woo-mmy.php
+++ woo-mmy.php
@@ -161,3 +161,65 @@ function save_field( $post_id, $post ) {
 }
 
 add_action( 'woocommerce_process_product_meta', 'save_field', 10, 2 );
+
+/**
+ * Custom shortcode to add year, make, model search form
+ */
+function make_model_year_shortcode() {
+	
+	return '
+		<form id="make-model-year-form" action="' . esc_url( get_permalink( wc_get_page_id( 'shop' ) ) ) .  '" method="get">
+			<div class="container">
+				<select id="make" name="make" required>
+					<option value="">Make</option>'. get_make_options() .
+				'</select>
+				<select id="model" name="model" required>
+					<option value="">Model</option>
+				</select>
+				<select id="car-year" name="car-year" required>
+					<option value="">Year</option>
+				</select>
+			</div>
+			<div class="wp-block-buttons is-layout-flex wp-block-buttons-is-layout-flex">
+				<div class="wp-block-button"><button class="wp-block-button__link wp-element-button" type="submit">Search</button></div>
+			</div>
+		</form>
+	';
+}
+
+add_shortcode('make-model-year-form', 'make_model_year_shortcode');
+
+function get_make_options() {
+	$terms = get_terms( array( 
+		'taxonomy' => 'woommy-car-details',
+	) );
+
+	//iterate over the custom taxonomy terms array and build an array of make names that have child models with the specified year
+	$makes = array();
+	$make_options = '';
+	foreach ( $terms as $term ) {
+		if ( 0 === $term->parent ) {
+			$make = array(
+				'slug' => $term->slug,
+				'name' => $term->name
+			);
+			if ( $make && ! in_array( $make["slug"], array_column( $makes, "slug" ) ) ) {
+				array_push( $makes, $make );
+			}
+		}
+		
+	}
+
+	foreach( $makes as $make ) {
+
+		$make_is_selected = '';
+		 
+		if( is_shop() && isset( $_GET['make'] ) && $make["slug"] === $_GET['make'] ) {
+			$make_is_selected = 'selected'; 
+		}
+		
+		$make_options .= '<option value="' . $make["slug"] . '" ' . $make_is_selected . '>' . $make["name"] . '</option>';
+	}
+
+	return $make_options;
+ }
