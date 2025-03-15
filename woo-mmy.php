7303178163cb91a4e6458227c29772c17e7a0acc
ajsmith3005 <135562778+ajsmith3005@users.noreply.github.com>
2025-03-15 00:54:19 -0700

    adds a constant for plugin version

--- woo-mmy.php
+++ woo-mmy.php
@@ -2,7 +2,7 @@
 /*
  * Plugin Name:       WooMMY
  * Description:       Search products by Make, Model, and Year by using custom taxonomies to categorize them.
- * Version:           0.1.0
+ * Version:           0.2.0
  * Author:            Andrew Smith
  * Author URI:        https://andycodes.net/
  * License:           GPL v3 or later
@@ -14,7 +14,10 @@
 	exit; // Exit if accessed directly
 }
 
+define( 'WOOMMY_VERSION', '0.2.0' );
+
 define( 'WOOMMY_PLUGIN' , __FILE__ );
+
 define( 'WOOMMY_PLUGIN_DIR', untrailingslashit( dirname( WOOMMY_PLUGIN) ) );
 
 require_once( WOOMMY_PLUGIN_DIR . '/includes/controller.php' );
