80dbc230f467489af3d1c8bd4fc8d995d27d2c75
ajsmith3005 <135562778+ajsmith3005@users.noreply.github.com>
2025-06-08 13:02:43 -0700

    biulds the block that will be displayed in editor and adds basic editor controls

--- blocks/mmy-form/src/mmy-form/edit.js
+++ blocks/mmy-form/src/mmy-form/edit.js
@@ -11,7 +11,12 @@ import { __ } from '@wordpress/i18n';
  *
  * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
  */
-import { useBlockProps } from '@wordpress/block-editor';
+import { InspectorControls, useBlockProps } from '@wordpress/block-editor';
+
+/**
+ * React components used to add interactive elements in the editor.
+ */
+import { PanelBody, TextControl } from '@wordpress/components';
 
 /**
  * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
@@ -29,10 +34,42 @@ import './editor.scss';
  *
  * @return {Element} Element to render.
  */
-export default function Edit() {
+export default function Edit( { attributes, setAttributes }) {
+	const { submitText } = attributes;
+	let displaySubmitText = submitText !== '' ? submitText : 'Search';
+
 	return (
-		<p { ...useBlockProps() }>
-			{ __( 'Mmy Form – hello from the editor!', 'mmy-form' ) }
-		</p>
+		<>
+			<InspectorControls>
+				<PanelBody title={ __( 'Settings', 'woommy-mmy-block' ) }>
+					<TextControl
+						label={ __(
+							'Submit Text',
+							'woommy-mmy-block'
+						)}
+						value={ submitText }
+						onChange={ ( value ) =>
+							setAttributes( { submitText: value})
+						}
+					/>
+				</PanelBody>
+			</InspectorControls>
+			<form id="make-model-year-form" method="get" {...useBlockProps()}>
+				<div class="container">
+					<select id="make" name="make" required>
+						<option value="">Make</option>
+					</select>
+					<select id="model" name="model" required>
+						<option value="">Model</option>
+					</select>
+					<select id="car-year" name="car-year" required>
+						<option value="">Year</option>
+					</select>
+				</div>
+				<div class="wp-block-buttons is-layout-flex wp-block-buttons-is-layout-flex">
+					<div class="wp-block-button"><button class="wp-block-button__link wp-element-button" type="submit">{ displaySubmitText }</button></div>
+				</div>
+			</form>
+		</>
 	);
 }
